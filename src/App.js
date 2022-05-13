b52b7005fa7ac09372441ecf8c143fe0c2f9750a
Minh Pham <43801761+phamduyanminh@users.noreply.github.com>
2022-05-12 22:20:28 -0400

    First initialize and fetching geoLocation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SeasonDisplay from './components/SeasonDisplay'
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    //This is the ONLY TIME we do direct assignment
+    //to this.state
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //Call setState for updating
+        //NEVER USE this.state.lat
+        this.setState({lat: position.coords.latitude});
+      },
+      error => {
+        this.setState({errorMessage: error.message})
+      }
+    );
+  }
+
+
+  render() {
+    //Conditional return
+    if(this.state.errorMessage && !this.state.lat){
+      return(
+        <div>
+          Error: {this.state.errorMessage}
+        </div>
+      )
+    }
+
+    if(!this.state.errorMessage && this.state.lat){
+      return(
+        <div>
+          Latitude: {this.state.lat}
+        </div>
+      )
+    }
+
+    return <div>Loading...</div>
+  }
+
 }
 
 export default App;
